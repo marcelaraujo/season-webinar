eb58001404ee0fcd7675a86ff029cb92e45386bf
Marcel Araujo <ceceldada@gmail.com>
2017-02-15 14:57:06 -0200

    Fix

--- application/views/helpers/ShowErrors.php
+++ application/views/helpers/ShowErrors.php
@@ -1,23 +1,5 @@
 <?php
-/**
- * InPhonex Client Application
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://www.inphonex.com/license/new-bsd
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@zend.com so we can send you a copy immediately.
- *
- * @category   Application
- * @package    View
- * @subpackage Helper
- * @copyright  Copyright (c) 2003-2011 InPhonex. (http://www.inphonex.com)
- * @license    http://www.inphonex.com/license/new-bsd     New BSD License
- */
+
 class Application_View_Helper_ShowErrors extends Zend_View_Helper_Abstract
 {
 	public function showErrors( $errors = NULL )
